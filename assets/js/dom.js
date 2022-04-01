d07e3f6656d67852c2d89f1e3b339f5805aa81e2
unknown <SENA@IBAAPRRTLFSP172.FormacionSena.RED>
2022-04-01 06:06:25 -0500

    first commit

---
+++ assets/js/dom.js
@@ -0,0 +1,41 @@
+// const nombre = document.getElementsByClassName("parrafo");
+// nombre.textContent = "Pepito Pérez"
+// let nuevoNombre = prompt("Ingrese un nombre");
+// nombre.textContent = nuevoNombre;
+// const nombre = document.getElementById("nombre");
+// const sol = document.getElementById("sol");
+// let numero = prompt("Ingrese un numero");
+// let a = 2;
+// let suma = parseInt(numero) + a;
+// alert(suma);
+// sol.textContent = suma;
+// console.log(sol);
+//TODO: Consultar los operadores relacionales ==, ===, !=, !==
+//TODO: Consultar los operadores lógicos AND, OR, NOT
+
+//Comparación
+console.log(1==2);
+console.log(2==2);
+console.log(3==4);
+//Igualdad estricta
+console.log(1 === 1);
+console.log('1' ===  1);
+let x=1;
+let y=1;
+console.log(x === y);
+//Diferencia
+console.log(1!=2);
+console.log(2!=2);
+console.log(5!=0);
+//OR 
+console.log(1||0);
+console.log(null || 0);
+console.log(true|| '')
+// AND
+console.log(1 && 0);
+console.log(null && undefinded);
+console.log(true && 2);
+//NOT
+console.log(!0);
+console.log(!undefinded);
+console.log(!2);
